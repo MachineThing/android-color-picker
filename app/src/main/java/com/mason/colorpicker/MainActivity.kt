df618710428a72ef9a3bea4309f8f325f0b2c22a
Mason Fisher <sackzombie10@gmail.com>
2020-11-14 22:55:39 -0500

    Got entire background color to change however it doesn't look that great

--- app/src/main/java/com/mason/colorpicker/MainActivity.kt
+++ app/src/main/java/com/mason/colorpicker/MainActivity.kt
@@ -3,9 +3,19 @@ package com.mason.colorpicker
 import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.RelativeLayout
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
+fun intToHex(int: Int):String {
+    var hexCol = Integer.toHexString(int)
+    if (hexCol.length == 1) {
+        return "0$hexCol"
+    } else {
+        return hexCol
+    }
+}
 
 class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
 
@@ -29,25 +39,35 @@ class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
 
     @SuppressLint("SetTextI18n")
     override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-        if (fromUser) { // Check if it's actually from the user
-            if (seekBar == findViewById<SeekBar>(R.id.seekR)) { // If it's the red slider
-                redCol = progress
-            } else if (seekBar == findViewById<SeekBar>(R.id.seekG)) { // If it's the green slider
-                greenCol = progress
-            } else { // If it's the blue slider
-                blueCol = progress
-            }
-            val hexText = findViewById<TextView>(R.id.hexView)
-            hexText.text = "#" + Integer.toHexString(redCol) + Integer.toHexString(greenCol) + Integer.toHexString(blueCol)
-            hexText.setBackgroundColor(Color.argb(255, redCol, greenCol, blueCol))
-            // Check if we need to make text color white
-            if (redCol < 113 && greenCol < 113 && blueCol < 113) {
-                // Color is dark, use white
-                hexText.setTextColor(Color.argb(255, 255, 255, 255))
-            } else {
-                // Color is light, use black
-                hexText.setTextColor(Color.argb(255, 0, 0, 0))
-            }
+        if (seekBar == findViewById<SeekBar>(R.id.seekR)) { // If it's the red slider
+            redCol = progress
+        } else if (seekBar == findViewById<SeekBar>(R.id.seekG)) { // If it's the green slider
+            greenCol = progress
+        } else { // If it's the blue slider
+            blueCol = progress
+        }
+        val hexText = findViewById<TextView>(R.id.hexView)
+        val colorView = findViewById<RelativeLayout>(R.id.colorView)
+        val redText = findViewById<TextView>(R.id.labelR)
+        val greenText = findViewById<TextView>(R.id.labelG)
+        val blueText = findViewById<TextView>(R.id.labelB)
+        hexText.text = "#${intToHex(redCol)}${intToHex(greenCol)}${intToHex(blueCol)}"
+        hexText.setBackgroundColor(Color.argb(255, redCol, greenCol, blueCol))
+        colorView.setBackgroundColor(Color.argb(255, redCol, greenCol, blueCol))
+
+        // Check if we need to make text color white
+        if (redCol < 128 && greenCol < 128 && blueCol < 128) {
+            // Color is dark, use white
+            hexText.setTextColor(Color.argb(255, 255, 255, 255))
+            redText.setTextColor(Color.argb(255, 255, 255, 255))
+            greenText.setTextColor(Color.argb(255, 255, 255, 255))
+            blueText.setTextColor(Color.argb(255, 255, 255, 255))
+        } else {
+            // Color is light, use black
+            hexText.setTextColor(Color.argb(255, 0, 0, 0))
+            redText.setTextColor(Color.argb(255, 0, 0, 0))
+            greenText.setTextColor(Color.argb(255, 0, 0, 0))
+            blueText.setTextColor(Color.argb(255, 0, 0, 0))
         }
     }
 
