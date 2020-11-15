ef8bb1d657cca39ad7e3fddff65b184fd363da51
Mason Fisher <sackzombie10@gmail.com>
2020-11-14 23:26:52 -0500

    Fixing code warnings

--- app/src/main/java/com/mason/colorpicker/MainActivity.kt
+++ app/src/main/java/com/mason/colorpicker/MainActivity.kt
@@ -9,19 +9,19 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 fun intToHex(int: Int):String {
-    var hexCol = Integer.toHexString(int)
-    if (hexCol.length == 1) {
-        return "0$hexCol"
+    val hexCol = Integer.toHexString(int)
+    return if (hexCol.length == 1) {
+        "0$hexCol"
     } else {
-        return hexCol
+        hexCol
     }
 }
 
 class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
 
-    var redCol = 255
-    var greenCol = 255
-    var blueCol = 255
+    private var redCol = 255
+    private var greenCol = 255
+    private var blueCol = 255
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,12 +39,16 @@ class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
 
     @SuppressLint("SetTextI18n")
     override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-        if (seekBar == findViewById<SeekBar>(R.id.seekR)) { // If it's the red slider
-            redCol = progress
-        } else if (seekBar == findViewById<SeekBar>(R.id.seekG)) { // If it's the green slider
-            greenCol = progress
-        } else { // If it's the blue slider
-            blueCol = progress
+        when (seekBar) {
+            findViewById<SeekBar>(R.id.seekR) -> { // If it's the red slider
+                redCol = progress
+            }
+            findViewById<SeekBar>(R.id.seekG) -> { // If it's the green slider
+                greenCol = progress
+            }
+            else -> { // If it's the blue slider
+                blueCol = progress
+            }
         }
         val hexText = findViewById<TextView>(R.id.hexView)
         val colorView = findViewById<RelativeLayout>(R.id.colorView)
